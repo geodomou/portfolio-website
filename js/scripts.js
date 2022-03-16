196dd87f2b992f0dc19b3917034e36deab2175ff
geodomou <99877522+geodomou@users.noreply.github.com>
2022-03-16 14:55:57 +0100

    JS Hamburger menu + Scrolldown

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("Hello and welcome to my Portfolio. :)")
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+   links.forEach(link => {
+      link.classList.toggle("fade");
+   });
+});
